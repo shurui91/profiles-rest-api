ace02614e939661532de44690780b00f3c43c692
Aaron Liu <shurui91@gmail.com>
2023-03-21 15:04:19 -0700

    admin page updated

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from profiles_api import models
 
-# Register your models here.
+# Register your models so it can show on the admin page
 admin.site.register(models.UserProfile)
