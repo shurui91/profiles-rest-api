86b2c6f8005ee7a7978ad9907017c680dd7390ea
Aaron Liu <shurui91@gmail.com>
2023-03-20 21:04:15 -0700

    models.py file updated

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -14,20 +14,16 @@ class UserProfileManager(BaseUserManager):
 
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
-
         user.set_password(password)
         user.save(using=self._db)
-
         return user
 
     def create_superuser(self, email, name, password):
         """create and save a superuser with given details"""
         user = self.create_user(email, name, password)
-
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
-
         return user
 
 
